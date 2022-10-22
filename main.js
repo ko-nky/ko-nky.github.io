a1815c6e28041eccb3fefb2ba7020a0e261ba93c
ko-nky <koasama2000@gmail.com>
2022-10-22 16:11:42 +0900

    Initial commit

--- main.js
+++ main.js
@@ -3,29 +3,3 @@ const viewer = new Cesium.Viewer('cesiumContainer', {
   terrainProvider: Cesium.createWorldTerrain(),
   baseLayerPicker: false,
 });
-const scene = viewer.scene;
-const primitives = scene.primitives;
-
-const baseTileset = new Cesium.Cesium3DTileset({
-  url: Cesium.IonResource.fromAssetId(1363490)
-});
-const colTileset = new Cesium.Cesium3DTileset({
-  url: Cesium.IonResource.fromAssetId(1363491)
-});
-primitives.add(baseTileset);
-primitives.add(colTileset);
-viewer.zoomTo(baseTileset, new Cesium.HeadingPitchRange(0, -0.5, 0));
-
-Sandcastle.addToggleButton("Show heatmap", true, function (
-  checked
-) {
-  var picking = checked;
-    if (picking){
-      baseTileset.show = checked;
-      colTileset.show = checked;
-    } else {
-      baseTileset.show = !checked;
-      colTileset.show = checked;
-   }
-});
-
